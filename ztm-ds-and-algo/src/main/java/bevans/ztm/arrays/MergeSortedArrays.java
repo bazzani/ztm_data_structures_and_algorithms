dad2851154b5456df7211c112e22587d81fafce3
Barry Evans <barryevans80@gmail.com>
2024-01-18 11:31:20 +0900

    Add Merge Sorted Arrays - ZTM impl

--- ztm-ds-and-algo/src/main/java/bevans/ztm/arrays/MergeSortedArrays.java
+++ ztm-ds-and-algo/src/main/java/bevans/ztm/arrays/MergeSortedArrays.java
@@ -1,31 +1,40 @@
 package bevans.ztm.arrays;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Objects;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class MergeSortedArrays {
-    Object[] merge(Object[] array1, Object[] array2) {
-        Object[] mergedArray = new Object[array1.length + array2.length];
+    Integer[] merge(Integer[] array1, Integer[] array2) {
 
-        Set<Object> array2Values = new HashSet<>();
-        for (int i = 0; i < array2.length; i++) {
-            array2Values.add(array2[i]);
+        if (array2.length == 0) {
+            return array1;
+        }
+        if (array1.length == 0) {
+            return array2;
         }
 
-        var mergedValueCounter = 0;
+        var mergedArray = new Integer[array1.length + array2.length];
+        var mergedItemCount = 0;
 
-        for (int i = 0; i < array1.length; i++) {
-            var value = array1[i];
-            mergedArray[mergedValueCounter] = value;
-            mergedValueCounter++;
+        var array1Index = 1;
+        var array1Item = array1[0];
+        var array2Index = 1;
+        var array2Item = array2[0];
 
-            if (array2Values.contains(value)) {
-                mergedArray[mergedValueCounter] = value;
-                mergedValueCounter++;
+        while (array1Item != null || array2Item != null) {
+            var useFirstArrayItem = array2Item == null || array1Item < array2Item;
+
+            if (useFirstArrayItem) {
+                mergedArray[mergedItemCount] = array1Item;
+                array1Item = array1Index < array1.length ? array1[array1Index] : null;
+                array1Index++;
+            } else {
+                mergedArray[mergedItemCount] = array2Item;
+                array2Item = array2Index < array2.length ? array2[array2Index] : null;
+                array2Index++;
             }
+            mergedItemCount++;
         }
 
         var mergedToString = Arrays.stream(mergedArray)
