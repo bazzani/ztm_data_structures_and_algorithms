851f9326752f6efd402d8d2abfbe30eba3650f90
Barry Evans <barryevans80@gmail.com>
2024-02-01 14:32:12 +0900

    Add LinkedList impl & tests - append/prepend impl...
    add commons-lang3 lib to use ReflectionToStringBuilder

--- ztm-ds-and-algo/src/main/java/bevans/ztm/linkedlist/LinkedList.java
+++ ztm-ds-and-algo/src/main/java/bevans/ztm/linkedlist/LinkedList.java
@@ -1,5 +1,8 @@
 package bevans.ztm.linkedlist;
 
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 public class LinkedList {
     private Node head;
     private Node tail;
@@ -14,14 +17,37 @@ public class LinkedList {
     public int length() {
         return length;
     }
-}
 
-class Node {
-    Object value;
-    Node next;
+    public void append(Object value) {
+        var newNode = new Node(value, null);
+        head.next = newNode;
+        tail = newNode;
+        length++;
+    }
 
-    public Node(Object value, Node next) {
-        this.value = value;
-        this.next = next;
+    public void prepend(Object value) {
+        var newNode = new Node(value, head);
+        head = newNode;
+        length++;
+    }
+
+    @Override
+    public String toString() {
+        return new ReflectionToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE).toString();
+    }
+
+    private class Node {
+        Object value;
+        Node next;
+
+        public Node(Object value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return new ReflectionToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).toString();
+        }
     }
 }
--- ztm-ds-and-algo/src/test/java/bevans/ztm/linkedlist/LinkedListTest.java
+++ ztm-ds-and-algo/src/test/java/bevans/ztm/linkedlist/LinkedListTest.java
@@ -1,20 +1,46 @@
 package bevans.ztm.linkedlist;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class LinkedListTest {
 
     @Test
-    void shouldCreateWithOneNode() {
+    void shouldCreateWithOneValue() {
         // given
         // when
         var sut = new LinkedList(10);
 
         // then
         assertThat(sut.length()).isEqualTo(1);
+        System.out.println("sut = " + sut);
+    }
+
+    @Test
+    void shouldAppendValue() {
+        // given
+        var sut = new LinkedList(10);
+
+        // when
+        sut.append(5);
+
+        // then
+        assertThat(sut.length()).isEqualTo(2);
+        System.out.println("sut = " + sut);
+    }
+
+    @Test
+    void shouldPrependValue() {
+        // given
+        var sut = new LinkedList(10);
+
+        // when
+        sut.prepend(5);
+
+        // then
+        assertThat(sut.length()).isEqualTo(2);
+
+        System.out.println("sut = " + sut);
     }
 }
