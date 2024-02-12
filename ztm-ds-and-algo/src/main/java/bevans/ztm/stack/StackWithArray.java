2e0ea1452c516131403dadef92e2700aaea7774e
Barry Evans <barryevans80@gmail.com>
2024-02-12 14:58:06 +0900

    Stack - Add array impl and tests

--- ztm-ds-and-algo/src/main/java/bevans/ztm/stack/StackWithArray.java
+++ ztm-ds-and-algo/src/main/java/bevans/ztm/stack/StackWithArray.java
@@ -1,26 +1,46 @@
 package bevans.ztm.stack;
 
+import java.util.Arrays;
 import java.util.StringJoiner;
 
 public class StackWithArray {
 
+    Object[] data;
+
+    public StackWithArray() {
+        this.data = new Object[]{};
+    }
+
     public Object peek() {
-        return null;
+        if (data.length == 0) {
+            return null;
+        } else {
+            return data[data.length - 1];
+        }
     }
 
     public void push(Object value) {
+        data = Arrays.copyOf(data, data.length + 1);
+        data[data.length - 1] = value;
     }
 
     public Object pop() {
-        return null;
+        var popped = data[data.length - 1];
+        data = Arrays.copyOf(data, data.length - 1);
+
+        return popped;
     }
 
     public boolean isEmpty() {
-        return true;
+        return data.length == 0;
     }
 
     public String printStack() {
         var joiner = new StringJoiner(",", "[", "]");
+
+        for (int i = data.length - 1; i >= 0; i--) {
+            joiner.add(data[i].toString());
+        }
 
         return joiner.toString();
     }
---
+++ ztm-ds-and-algo/src/test/java/bevans/ztm/stack/StackWithArrayTest.java
@@ -0,0 +1,88 @@
+package bevans.ztm.stack;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StackWithArrayTest {
+
+    private StackWithArray sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new StackWithArray();
+    }
+
+    @Test
+    void shouldPeek() {
+        // given
+        sut.push("google");
+        sut.push("Udemy");
+
+        // when
+        var peeked = sut.peek();
+
+        // then
+        assertThat(peeked).isEqualTo("Udemy");
+        System.out.println("stack = " + sut.printStack());
+    }
+
+    @Test
+    void shouldPeekEmptyStack() {
+        // given
+        // when
+        var peeked = sut.peek();
+
+        // then
+        assertThat(peeked).isNull();
+        System.out.println("stack = " + sut.printStack());
+    }
+
+    @Test
+    void shouldPush() {
+        // given
+        // when
+        sut.push("google");
+        sut.push("Udemy");
+
+        // then
+        assertThat(sut.isEmpty()).isFalse();
+        assertThat(sut.peek()).isEqualTo("Udemy");
+        System.out.println("stack = " + sut.printStack());
+    }
+
+    @Test
+    void shouldPop() {
+        // given
+        sut.push("google");
+        sut.push("Udemy.com");
+        sut.push("ZTM.com");
+
+        // when
+        var popped = sut.pop();
+
+        // then
+        assertThat(popped).isEqualTo("ZTM.com");
+        assertThat(sut.isEmpty()).isFalse();
+        System.out.println("stack = " + sut.printStack());
+    }
+
+    @Test
+    void shouldPopUntilEmpty() {
+        // given
+        sut.push("google");
+        sut.push("Udemy.com");
+        sut.push("ZTM.com");
+        sut.pop();
+        sut.pop();
+
+        // when
+        var popped = sut.pop();
+
+        // then
+        assertThat(popped).isEqualTo("google");
+        assertThat(sut.isEmpty()).isTrue();
+        System.out.println("stack = " + sut.printStack());
+    }
+}
