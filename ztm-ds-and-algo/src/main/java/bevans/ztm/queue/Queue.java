4e7ae8b6b1b3b6ae0b81668ce4b35f42e5d77188
Barry Evans <barryevans80@gmail.com>
2024-02-12 17:23:34 +0900

    Queue - Add interface

---
+++ ztm-ds-and-algo/src/main/java/bevans/ztm/queue/Queue.java
@@ -0,0 +1,45 @@
+package bevans.ztm.queue;
+
+import java.util.StringJoiner;
+
+public class Queue {
+    private Node first;
+    private Node last;
+    private int length;
+
+    public Object peek() {
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    public void enqueue(String value) {
+    }
+
+    public Object dequeue() {
+        return null;
+    }
+
+    public String printQueue() {
+        var joiner = new StringJoiner(",", "[", "]");
+
+        var currentNode = first;
+        while (currentNode != null) {
+            joiner.add(currentNode.value.toString());
+            currentNode = currentNode.next;
+        }
+
+        return joiner.toString();
+    }
+
+    private static class Node {
+        private Object value;
+        private Node next;
+
+        public Node(Object value) {
+            this.value = value;
+        }
+    }
+}
