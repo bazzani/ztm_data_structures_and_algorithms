608ca7588e79a805b5e1a38486ad1bcf04e4d9d5
Barry Evans <barryevans80@gmail.com>
2024-03-08 12:54:23 +0900

    Impl 2 - Remove InsertionSort impl

--- ztm-ds-and-algo/src/main/java/bevans/ztm/algo/sort/InsertionSort.java
+++ ztm-ds-and-algo/src/main/java/bevans/ztm/algo/sort/InsertionSort.java
@@ -4,19 +4,5 @@ public class InsertionSort {
     static int SWAP_COUNT = 0;
 
     public void sort(int[] array) {
-        var arrayLength = array.length;
-
-        for (int i = 1; i < arrayLength; i++) {
-            var current = i;
-
-            while (current > 0 && array[current] < array[current - 1]) {
-                var temp = array[current - 1];
-                array[current - 1] = array[current];
-                array[current] = temp;
-                current--;
-
-                SWAP_COUNT++;
-            }
-        }
     }
 }
