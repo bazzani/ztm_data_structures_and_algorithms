315810f92506d595a60bca1b41aa55904af6fa4f
Barry Evans <barryevans80@gmail.com>
2024-03-08 12:15:07 +0900

    Impl 2 - Remove HashTable impl

--- ztm-ds-and-algo/src/main/java/bevans/ztm/hashtable/HashTable.java
+++ ztm-ds-and-algo/src/main/java/bevans/ztm/hashtable/HashTable.java
@@ -1,7 +1,6 @@
 package bevans.ztm.hashtable;
 
 import java.util.Arrays;
-import java.util.HashSet;
 
 public class HashTable {
     private final Object[][][] data;
@@ -11,96 +10,18 @@ public class HashTable {
     }
 
     void set(String key, Object value) {
-        var address = hash(key);
-        Object[][] bucket = data[address];
-
-        if (bucket == null) {
-            bucket = new Object[1][];
-            data[address] = bucket;
-        } else {
-            var keyIndex = getKeyIndex(key, bucket);
-
-            if (keyIndex > -1) {
-                bucket[keyIndex][1] = value;
-            } else {
-                bucket = createLargerBucket(bucket);
-                data[address] = bucket;
-            }
-        }
-
-        var newEntry = new Object[2];
-        newEntry[0] = key;
-        newEntry[1] = value;
-        bucket[bucket.length - 1] = newEntry;
-    }
-
-    private int getKeyIndex(String key, Object[][] bucket) {
-        var keyIndex = -1;
-
-        for (int i = 0; i < bucket.length; i++) {
-            if (bucket[i][0] == key) {
-                keyIndex = i;
-                break;
-            }
-        }
-
-        return keyIndex;
-    }
-
-    private Object[][] createLargerBucket(Object[][] bucket) {
-        var largerBucket = new Object[bucket.length + 1][];
-
-        System.arraycopy(bucket, 0,
-                largerBucket, 0,
-                bucket.length);
-
-        return largerBucket;
     }
 
     Object get(String key) {
-        Object value = null;
-
-        var address = hash(key);
-        Object[][] currentBucket = data[address];
-
-        if (currentBucket != null) {
-            for (int i = 0; i < currentBucket.length; i++) {
-                if (currentBucket[i][0].equals(key)) {
-                    value = currentBucket[i][1];
-                }
-            }
-        }
-
-        return value;
+        return null;
     }
 
     private int hash(String key) {
-        int hash = 0;
-
-        for (int i = 0; i < key.length(); i++) {
-            int charCode = key.charAt(i);
-            hash = (hash + charCode * i) % data.length;
-        }
-
-        return hash;
+        return 0;
     }
 
     String[] keys() {
-        var keySet = new HashSet<String>();
-
-        for (Object[][] currentBucket : data) {
-            if (currentBucket != null) {
-                if (currentBucket.length == 1) {
-                    keySet.add(currentBucket[0][0].toString());
-                } else {
-                    for (Object[] objects : currentBucket) {
-                        keySet.add(objects[0].toString());
-                    }
-                }
-            }
-        }
-
-        return keySet.toArray(new String[0]);
+        return null;
     }
 
     @Override
