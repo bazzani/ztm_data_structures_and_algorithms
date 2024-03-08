d440f1fdf086b6b17db4a7cfaae8acc1541e5cc0
Barry Evans <barryevans80@gmail.com>
2024-03-08 12:49:27 +0900

    Impl 2 - Remove Fibonacci impl

--- ztm-ds-and-algo/src/main/java/bevans/ztm/algo/recursion/Fibonacci.java
+++ ztm-ds-and-algo/src/main/java/bevans/ztm/algo/recursion/Fibonacci.java
@@ -6,32 +6,11 @@ import java.util.List;
 import java.util.Map;
 
 public class Fibonacci {
-    long iterations = 0;
-
-    private final Map<Integer, Integer> cache = new HashMap<>();
-
     int fibonacciRecursive(int index) {
-        iterations++;
-
-        if (cache.containsKey(index)) {
-            return cache.get(index);
-        } else {
-            if (index < 2) {
-                return index;
-            } else {
-                cache.put(index, fibonacciRecursive(index - 1) + fibonacciRecursive(index - 2));
-                return cache.get(index);
-            }
-        }
+        return 0;
     }
 
     int fibonacciIterative(int index) {
-        var arr = new ArrayList<>(List.of(0, 1));
-
-        for (int i = 2; i < index + 1; i++) {
-            arr.add(arr.get(i - 2) + arr.get(i - 1));
-        }
-
-        return arr.get(index);
+        return 0;
     }
 }
