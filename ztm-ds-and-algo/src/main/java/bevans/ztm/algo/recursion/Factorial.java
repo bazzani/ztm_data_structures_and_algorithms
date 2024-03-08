7f288b758492a01e50338d1df1170ddbfb2598fb
Barry Evans <barryevans80@gmail.com>
2024-03-08 12:49:16 +0900

    Impl 2 - Remove Factorial impl

--- ztm-ds-and-algo/src/main/java/bevans/ztm/algo/recursion/Factorial.java
+++ ztm-ds-and-algo/src/main/java/bevans/ztm/algo/recursion/Factorial.java
@@ -2,24 +2,10 @@ package bevans.ztm.algo.recursion;
 
 public class Factorial {
     public int factorialRecursive(int input) {
-        if (input == 0) {
-            return 1;
-        }
-
-        if (input == 2) {
-            return 2;
-        }
-
-        return input * factorialRecursive(input - 1);
+        return 0;
     }
 
     public int factorialIterative(int input) {
-        var result = 1;
-
-        for (int i = 2; i <= input; i++) {
-            result = result * i;
-        }
-
-        return result;
+        return 0;
     }
 }
