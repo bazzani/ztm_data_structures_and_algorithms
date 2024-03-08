f873b5a55f3e0eccf7281e0807eca70391dd944c
Barry Evans <barryevans80@gmail.com>
2024-03-08 13:00:11 +0900

    Impl 2 - Remove FirstRecurringCharacter impl

--- ztm-ds-and-algo/src/main/java/bevans/ztm/hashtable/FirstRecurringCharacter.java
+++ ztm-ds-and-algo/src/main/java/bevans/ztm/hashtable/FirstRecurringCharacter.java
@@ -2,20 +2,6 @@ package bevans.ztm.hashtable;
 
 class FirstRecurringCharacter {
     String findFirstRecurringCharacter(int[] input) {
-        var foundValues = new HashTable(10);
-        String recurringValue = null;
-
-        for (int i : input) {
-            var value = String.valueOf(i);
-
-            if (foundValues.get(value) != null) {
-                recurringValue = value;
-                break;
-            } else {
-                foundValues.set(value, value);
-            }
-        }
-
-        return recurringValue;
+        return null;
     }
 }
