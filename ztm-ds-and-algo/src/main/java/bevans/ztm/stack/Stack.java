0cdeaffcc2b2feb55723888e44a381589b78e269
Barry Evans <barryevans80@gmail.com>
2024-03-08 12:25:46 +0900

    Impl 2 - Remove Stack impl

--- ztm-ds-and-algo/src/main/java/bevans/ztm/stack/Stack.java
+++ ztm-ds-and-algo/src/main/java/bevans/ztm/stack/Stack.java
@@ -4,69 +4,27 @@ import java.util.StringJoiner;
 
 public class Stack {
 
-    private Node top;
-    private Node bottom;
-    private int length;
-
     public Object peek() {
-        if (isEmpty()) {
-            return null;
-        } else {
-            return top.value;
-        }
+        return null;
     }
 
     public void push(Object value) {
-        if (length == 0) {
-            var newNode = new Node(value, null);
-            top = newNode;
-            bottom = newNode;
-        } else {
-            var newNode = new Node(value, top);
-            top = newNode;
-        }
-        length++;
     }
 
     public Object pop() {
-        if (top == null) {
-            return null;
-        }
-
-        if (top == bottom) {
-            bottom = null;
-        }
-
-        var popped = top.value;
-        top = top.next;
-        length--;
-
-        return popped;
+        return null;
     }
 
     public boolean isEmpty() {
-        return length == 0;
+        return true;
     }
 
     public String printStack() {
         var joiner = new StringJoiner(",", "[", "]");
 
-        var current = top;
-        while (current != null) {
-            joiner.add(current.value.toString());
-            current = current.next;
-        }
-
         return joiner.toString();
     }
 
     private static class Node {
-        Object value;
-        Node next;
-
-        public Node(Object value, Node next) {
-            this.value = value;
-            this.next = next;
-        }
     }
 }
