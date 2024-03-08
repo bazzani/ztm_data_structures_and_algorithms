ae310f839dea312d423ce8a31729f5e052881866
Barry Evans <barryevans80@gmail.com>
2024-03-08 12:40:16 +0900

    Impl 2 - Remove LeetCodeQueueSolution2 impl

--- ztm-ds-and-algo/src/main/java/bevans/ztm/queue/LeetCodeQueueSolution2.java
+++ ztm-ds-and-algo/src/main/java/bevans/ztm/queue/LeetCodeQueueSolution2.java
@@ -1,49 +1,22 @@
 package bevans.ztm.queue;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 public class LeetCodeQueueSolution2 {
-    private Integer front;
-    private final Deque<Integer> stack1;
-    private final Deque<Integer> stack2;
-
     public LeetCodeQueueSolution2() {
-        this.stack1 = new ArrayDeque<>();
-        this.stack2 = new ArrayDeque<>();
     }
 
     public void push(int value) {
-        if (stack1.isEmpty()) {
-            front = value;
-        }
-
-        stack1.push(value);
     }
 
     public Integer pop() {
-        if (stack2.isEmpty()) {
-            while (!stack1.isEmpty()) {
-                stack2.push(stack1.pop());
-            }
-        }
-
-        if (stack2.isEmpty()) { // needed to avoid java.util.NoSuchElementException
-            return null;
-        } else {
-            return stack2.pop();
-        }
+        return null;
     }
 
     public boolean empty() {
-        return stack1.isEmpty() && stack2.isEmpty();
+        return true;
     }
 
     public Integer peek() {
-        if (!stack2.isEmpty()) {
-            return stack2.peek();
-        }
-        return front;
+        return null;
     }
 
 /**
@@ -53,5 +26,9 @@ public class LeetCodeQueueSolution2 {
  * int param_2 = obj.pop();
  * int param_3 = obj.peek();
  * boolean param_4 = obj.empty();
+ */
+
+/**
+ * pop() will move all items to stack2 if empty
  */
 }
