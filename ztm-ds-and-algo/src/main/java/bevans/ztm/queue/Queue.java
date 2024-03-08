3c0f8faabec72ff12678bffdd0e0bf1bb44e9977
Barry Evans <barryevans80@gmail.com>
2024-03-08 12:28:43 +0900

    Impl 2 - Remove Queue impl

--- ztm-ds-and-algo/src/main/java/bevans/ztm/queue/Queue.java
+++ ztm-ds-and-algo/src/main/java/bevans/ztm/queue/Queue.java
@@ -3,69 +3,27 @@ package bevans.ztm.queue;
 import java.util.StringJoiner;
 
 public class Queue {
-    private Node first;
-    private Node last;
-    private int length;
-
     public Object peek() {
-        if (length == 0) {
-            return null;
-        } else {
-            return first.value;
-        }
+        return null;
     }
 
     public boolean isEmpty() {
-        return length == 0;
+        return true;
     }
 
     public void enqueue(Object value) {
-        var newNode = new Node(value);
-
-        if (length == 0) {
-            first = newNode;
-            last = newNode;
-        } else {
-            last.next = newNode;
-            last = newNode;
-        }
-
-        length++;
     }
 
     public Object dequeue() {
-        if (first == null) {
-            return null;
-        }
-        if (first == last) {
-            last = null;
-        }
-
-        var dequeued = first;
-        first = first.next;
-        length--;
-
-        return dequeued.value;
+        return null;
     }
 
     public String printQueue() {
         var joiner = new StringJoiner(",", "[", "]");
 
-        var currentNode = first;
-        while (currentNode != null) {
-            joiner.add(currentNode.value.toString());
-            currentNode = currentNode.next;
-        }
-
         return joiner.toString();
     }
 
     private static class Node {
-        private Object value;
-        private Node next;
-
-        public Node(Object value) {
-            this.value = value;
-        }
     }
 }
