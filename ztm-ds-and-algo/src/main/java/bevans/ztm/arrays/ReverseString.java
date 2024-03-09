3db3a8c0b25827511618499fd2b9f2c470ffc81d
Barry Evans <barryevans80@gmail.com>
2024-03-09 15:18:05 +0900

    Impl 2 - Add ReverseString impl

--- ztm-ds-and-algo/src/main/java/bevans/ztm/arrays/ReverseString.java
+++ ztm-ds-and-algo/src/main/java/bevans/ztm/arrays/ReverseString.java
@@ -2,6 +2,13 @@ package bevans.ztm.arrays;
 
 public class ReverseString {
     public String reverse(String input) {
-        return null;
+        var chars = input.toCharArray();
+        var reversedChars = new char[chars.length];
+
+        for (int i = chars.length - 1; i >= 0; i--) {
+            reversedChars[chars.length - i - 1] = chars[i];
+        }
+
+        return new String(reversedChars);
     }
 }
