ae557beb4da1854e76072cdacfc8ef22974c6806
Barry Evans <barryevans80@gmail.com>
2024-03-09 15:57:41 +0900

    Impl 2 - MergeSortedArraysTest - add a few more assertions

--- ztm-ds-and-algo/src/test/java/bevans/ztm/arrays/MergeSortedArraysTest.java
+++ ztm-ds-and-algo/src/test/java/bevans/ztm/arrays/MergeSortedArraysTest.java
@@ -15,11 +15,67 @@ class MergeSortedArraysTest {
     }
 
     @Test
-    void shouldMergeSortedArrays() {
+    void shouldMergeSortedArraysSameSize() {
+        // given
+        var array1 = Arrays.array(0, 3, 4, 31);
+        var array2 = Arrays.array(4, 6, 30, 32);
+        var expectedArray = Arrays.array(0, 3, 4, 4, 6, 30, 31, 32);
+
+        // when
+        var mergedArray = sut.merge(array1, array2);
+
+        // then
+        assertArrayEquals(expectedArray, mergedArray);
+    }
+
+    @Test
+    void shouldMergeSortedArraysFirstIsLarger() {
         // given
         var array1 = Arrays.array(0, 3, 4, 31);
         var array2 = Arrays.array(4, 6, 30);
         var expectedArray = Arrays.array(0, 3, 4, 4, 6, 30, 31);
+
+        // when
+        var mergedArray = sut.merge(array1, array2);
+
+        // then
+        assertArrayEquals(expectedArray, mergedArray);
+    }
+
+    @Test
+    void shouldMergeSortedArraysSecondIsLarger() {
+        // given
+        var array1 = Arrays.array(0, 3, 4, 31);
+        var array2 = Arrays.array(4, 6, 10, 15, 30);
+        var expectedArray = Arrays.array(0, 3, 4, 4, 6, 10, 15, 30, 31);
+
+        // when
+        var mergedArray = sut.merge(array1, array2);
+
+        // then
+        assertArrayEquals(expectedArray, mergedArray);
+    }
+
+    @Test
+    void shouldMergeSortedArraysFirstIsEmpty() {
+        // given
+        var array1 = new Integer[]{};
+        var array2 = Arrays.array(4, 6, 30);
+        var expectedArray = Arrays.array(4, 6, 30);
+
+        // when
+        var mergedArray = sut.merge(array1, array2);
+
+        // then
+        assertArrayEquals(expectedArray, mergedArray);
+    }
+
+    @Test
+    void shouldMergeSortedArraysSecondIsEmpty() {
+        // given
+        var array1 = Arrays.array(0, 3, 4, 31);
+        var array2 = new Integer[]{};
+        var expectedArray = Arrays.array(0, 3, 4, 31);
 
         // when
         var mergedArray = sut.merge(array1, array2);
