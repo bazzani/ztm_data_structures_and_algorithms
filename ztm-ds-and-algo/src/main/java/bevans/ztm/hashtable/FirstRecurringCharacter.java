226782ef7c86104a648b8866a71b37c78da5e639
Barry Evans <barryevans80@gmail.com>
2024-03-12 15:32:46 +0900

    Impl 2 - Add FirstRecurringCharacter impl

--- ztm-ds-and-algo/src/main/java/bevans/ztm/hashtable/FirstRecurringCharacter.java
+++ ztm-ds-and-algo/src/main/java/bevans/ztm/hashtable/FirstRecurringCharacter.java
@@ -1,7 +1,18 @@
 package bevans.ztm.hashtable;
 
+import java.util.HashSet;
+
 class FirstRecurringCharacter {
     String findFirstRecurringCharacter(int[] input) {
+        var found = new HashSet<Integer>();
+
+        for (int chr : input) {
+            if (found.contains(chr)) {
+                return Integer.toString(chr);
+            }
+            found.add(chr);
+        }
+
         return null;
     }
 }
