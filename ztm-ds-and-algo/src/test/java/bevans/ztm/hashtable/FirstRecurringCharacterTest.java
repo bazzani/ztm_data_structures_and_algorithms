6154e685fe1e368e4ce24ae6785287726136c07a
Barry Evans <barryevans80@gmail.com>
2024-03-12 15:28:43 +0900

    Impl 2 - FirstRecurringCharacter - use @ParameterizedTest

--- ztm-ds-and-algo/src/test/java/bevans/ztm/hashtable/FirstRecurringCharacterTest.java
+++ ztm-ds-and-algo/src/test/java/bevans/ztm/hashtable/FirstRecurringCharacterTest.java
@@ -1,9 +1,14 @@
 package bevans.ztm.hashtable;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.of;
 
 class FirstRecurringCharacterTest {
 
@@ -14,33 +19,22 @@ class FirstRecurringCharacterTest {
         sut = new FirstRecurringCharacter();
     }
 
-    @Test
-    void shouldFindFirstRecurringCharacter2() {
+    @ParameterizedTest
+    @MethodSource("testData")
+    void shouldFindFirstRecurringCharacter(int[] data, String expected) {
         // given
         // when
-        var value = sut.findFirstRecurringCharacter(new int[]{2, 5, 1, 2, 3, 5, 1, 2, 4});
+        var found = sut.findFirstRecurringCharacter(data);
 
         // then
-        assertThat(value).isEqualTo("2");
+        assertThat(found).isEqualTo(expected);
     }
 
-    @Test
-    void shouldFindFirstRecurringCharacter1() {
-        // given
-        // when
-        var value = sut.findFirstRecurringCharacter(new int[]{2, 1, 1, 2, 3, 5, 1, 2, 4});
-
-        // then
-        assertThat(value).isEqualTo("1");
-    }
-
-    @Test
-    void shouldNotFindFirstRecurringCharacter() {
-        // given
-        // when
-        var value = sut.findFirstRecurringCharacter(new int[]{2, 3, 4, 5});
-
-        // then
-        assertThat(value).isNull();
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                of(new int[]{2, 5, 1, 2, 3, 5, 1, 2, 4}, "2"),
+                of(new int[]{2, 1, 1, 2, 3, 5, 1, 2, 4}, "1"),
+                of(new int[]{2, 3, 4, 5}, null)
+        );
     }
 }
