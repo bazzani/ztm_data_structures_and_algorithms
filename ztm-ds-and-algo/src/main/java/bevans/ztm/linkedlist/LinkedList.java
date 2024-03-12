9dd0b039c05a744ecff7abedf463abbf83ad19f2
Barry Evans <barryevans80@gmail.com>
2024-03-13 08:03:19 +0900

    Revert "Impl 2 - Add LinkedList impl"
    This reverts commit cf7280fe42029198b36722e1ac05ce99c88d664e.

--- ztm-ds-and-algo/src/main/java/bevans/ztm/linkedlist/LinkedList.java
+++ ztm-ds-and-algo/src/main/java/bevans/ztm/linkedlist/LinkedList.java
@@ -2,106 +2,36 @@ package bevans.ztm.linkedlist;
 
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+import org.w3c.dom.Node;
 
 import java.util.StringJoiner;
 
 public class LinkedList {
 
-    private int length;
-    private Node head;
-    private Node tail;
-
     public LinkedList() {
     }
 
     public LinkedList(Object value) {
-        this.head = new Node(value);
-        this.tail = head;
-        this.length = 1;
     }
 
     public int length() {
-        return length;
+        return 0;
     }
 
     public void append(Object value) {
-        var newNode = new Node(value);
-        tail.next = newNode;
-        tail = newNode;
-
-        length++;
     }
 
     public void prepend(Object value) {
-        head = new Node(value, head);
-        length++;
     }
 
     public void insert(int index, Object value) {
-        if (index >= length) {
-            append(value);
-            return;
-        }
-        if (index == 0) {
-            prepend(value);
-            return;
-        }
-
-        var leader = traverseToIndex(index - 1);
-        leader.next = new Node(value, leader.next);
-        length++;
     }
 
     public void remove(int index) {
-        if (index == 0) {
-            var unwantedNode = traverseToIndex(index);
-            head = unwantedNode.next;
-            length--;
-
-            if (length == index) {
-                tail = head;
-            }
-        } else {
-            var leader = traverseToIndex(index - 1);
-            var unwantedNode = leader.next;
-            leader.next = unwantedNode.next;
-
-            length--;
-            if (length == index) {
-                tail = leader;
-            }
-        }
     }
 
     private Node traverseToIndex(int index) {
-        var indexCount = 0;
-        var currentNode = head;
-
-        while (indexCount != index) {
-            currentNode = currentNode.next;
-            indexCount++;
-        }
-
-        return currentNode;
-    }
-
-    public void reverse() {
-        var emptyOrSingleItemList = this.head == null || this.head.next == null;
-        if (!emptyOrSingleItemList) {
-            var first = head;
-            tail = head;
-            var second = first.next;
-
-            while (second != null) {
-                var temp = second.next;
-                second.next = first;
-                first = second;
-                second = temp;
-            }
-
-            head.next = null;
-            head = first;
-        }
+        return null;
     }
 
     @Override
@@ -111,32 +41,24 @@ public class LinkedList {
 
     String printList() {
         var joiner = new StringJoiner(",", "[", "]");
-
-        var currentNode = head;
-        while (currentNode != null) {
-            joiner.add(currentNode.value.toString());
-            currentNode = currentNode.next;
-        }
-
         return joiner.toString();
     }
 
+    public LinkedList reverse() {
+        return null;
+    }
+
+    public LinkedList reverseZtm() {
+        return null;
+    }
+
+    public void reverseBarry() {
+
+    }
+
+    public void reverseBarryTwo() {
+    }
+
     private static class Node {
-        Object value;
-        Node next;
-
-        public Node(Object value) {
-            this.value = value;
-        }
-
-        public Node(Object value, Node next) {
-            this.value = value;
-            this.next = next;
-        }
-
-        @Override
-        public String toString() {
-            return ReflectionToStringBuilder.toString(this);
-        }
     }
 }
