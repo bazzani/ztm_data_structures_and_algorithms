5f716d35108971e4333bb2bcd2e334b38a3fc736
Barry Evans <barryevans80@gmail.com>
2024-03-13 09:18:35 +0900

    Impl 2 - Add LinkedList impl - take 2

--- ztm-ds-and-algo/src/main/java/bevans/ztm/linkedlist/LinkedList.java
+++ ztm-ds-and-algo/src/main/java/bevans/ztm/linkedlist/LinkedList.java
@@ -2,36 +2,94 @@ package bevans.ztm.linkedlist;
 
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
-import org.w3c.dom.Node;
 
 import java.util.StringJoiner;
 
 public class LinkedList {
 
+    private Node head;
+    private Node tail;
+    private int length;
+
     public LinkedList() {
     }
 
     public LinkedList(Object value) {
+        this.head = new Node(value);
+        this.tail = head;
+        this.length = 1;
     }
 
     public int length() {
-        return 0;
+        return length;
     }
 
     public void append(Object value) {
+        var newNode = new Node(value);
+
+        if (tail != null) {
+            tail.next = newNode;
+        }
+
+        tail = newNode;
+        length++;
     }
 
     public void prepend(Object value) {
+        head = new Node(value, head);
+        length++;
     }
 
     public void insert(int index, Object value) {
+        if (index == 0) {
+            prepend(value);
+        } else if (index >= length) {
+            append(value);
+        } else {
+            var leader = traverseToIndex(index - 1);
+            var temp = leader.next;
+            leader.next = new Node(value, temp);
+            length++;
+        }
     }
 
     public void remove(int index) {
+        if (index == 0) {
+            var unwantedNode = head;
+            head = unwantedNode.next;
+            unwantedNode.next = null;
+            length--;
+        } else {
+            var leader = traverseToIndex(index - 1);
+            var unwantedNode = leader.next;
+            leader.next = unwantedNode.next;
+            tail = leader.next;
+
+            length--;
+            if (length == index) {
+                tail = leader;
+            }
+        }
+
+        if (length == 1) {
+            tail = head;
+        }
+        if (length == 0) {
+            tail = null;
+            head = null;
+        }
     }
 
     private Node traverseToIndex(int index) {
-        return null;
+        var currentIndex = 0;
+        var currentNode = head;
+
+        while (currentIndex != index) {
+            currentNode = currentNode.next;
+            currentIndex++;
+        }
+
+        return currentNode;
     }
 
     @Override
@@ -41,24 +99,52 @@ public class LinkedList {
 
     String printList() {
         var joiner = new StringJoiner(",", "[", "]");
+
+        var currentNode = head;
+        while (currentNode != null) {
+            joiner.add(currentNode.value.toString());
+            currentNode = currentNode.next;
+        }
+
         return joiner.toString();
     }
 
-    public LinkedList reverse() {
-        return null;
-    }
+    public void reverse() {
+        var emptyOrSingleItemList = length <= 1;
 
-    public LinkedList reverseZtm() {
-        return null;
-    }
+        if (!emptyOrSingleItemList) {
+            tail = head;
+            var first = head;
+            var second = first.next;
 
-    public void reverseBarry() {
+            while (second != null) {
+                var temp = second.next;
+                second.next = first;
+                first = second;
+                second = temp;
+            }
 
-    }
-
-    public void reverseBarryTwo() {
+            head.next = null;
+            head = first;
+        }
     }
 
     private static class Node {
+        Object value;
+        Node next;
+
+        Node(Object value) {
+            this.value = value;
+        }
+
+        Node(Object value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return new ReflectionToStringBuilder(this, ToStringStyle.JSON_STYLE).toString();
+        }
     }
 }
