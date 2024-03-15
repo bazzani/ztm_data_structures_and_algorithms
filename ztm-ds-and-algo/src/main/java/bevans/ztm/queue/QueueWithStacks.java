e61d41dc406c8a863b128e77b6de87782ed5e424
Barry Evans <barryevans80@gmail.com>
2024-03-15 18:16:20 +0900

    Impl 2 - Add QueueWithStacks impl

--- ztm-ds-and-algo/src/main/java/bevans/ztm/queue/QueueWithStacks.java
+++ ztm-ds-and-algo/src/main/java/bevans/ztm/queue/QueueWithStacks.java
@@ -1,27 +1,50 @@
 package bevans.ztm.queue;
 
-import bevans.ztm.stack.StackWithArray;
+import bevans.ztm.stack.Stack;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 import java.util.StringJoiner;
 
 public class QueueWithStacks {
 
+    private Stack s1;
+    private Stack s2;
+    private Object head;
+
     public QueueWithStacks() {
+        s1 = new Stack();
+        s2 = new Stack();
     }
 
     public Object peek() {
-        return null;
+        if (!s2.isEmpty()) {
+            return s2.peek();
+        } else {
+            return head;
+        }
     }
 
     public boolean isEmpty() {
-        return true;
+        return s1.isEmpty() && s2.isEmpty();
     }
 
     public void enqueue(String value) {
+        if (s1.isEmpty()) {
+            head = value;
+        }
+
+        s1.push(value);
     }
 
     public Object dequeue() {
-        return null;
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+
+        return s2.isEmpty() ? null : s2.pop();
     }
 
     public String printQueue() {
@@ -30,5 +53,10 @@ public class QueueWithStacks {
         // todo fix printing of rest elements in queue
 
         return joiner.toString();
+    }
+
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.toString(this, ToStringStyle.JSON_STYLE);
     }
 }
