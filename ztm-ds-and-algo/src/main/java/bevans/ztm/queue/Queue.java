3acb47d5f1a7a48297e2f332f25ac0507b840b94
Barry Evans <barryevans80@gmail.com>
2024-03-15 12:07:14 +0900

    Impl 2 - Add Queue impl

--- ztm-ds-and-algo/src/main/java/bevans/ztm/queue/Queue.java
+++ ztm-ds-and-algo/src/main/java/bevans/ztm/queue/Queue.java
@@ -1,29 +1,84 @@
 package bevans.ztm.queue;
 
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 import java.util.StringJoiner;
 
 public class Queue {
+    Node head;
+    Node tail;
+    int length;
+
     public Object peek() {
-        return null;
+        if (head != null) {
+            return head.value;
+        } else {
+            return null;
+        }
     }
 
     public boolean isEmpty() {
-        return true;
+        return length == 0;
     }
 
     public void enqueue(Object value) {
+        var newNode = new Node(value);
+
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
     }
 
     public Object dequeue() {
-        return null;
+        if (head == null) {
+            return null;
+        }
+
+        var dequeued = head;
+        head = dequeued.next;
+        length--;
+
+        if (length == 0) {
+            tail = null;
+        }
+
+        return dequeued.value;
     }
 
     public String printQueue() {
         var joiner = new StringJoiner(",", "[", "]");
 
+        var currentNode = head;
+        while (currentNode != null) {
+            joiner.add(currentNode.value.toString());
+            currentNode = currentNode.next;
+        }
+
         return joiner.toString();
     }
 
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.toString(this, ToStringStyle.JSON_STYLE);
+    }
+
     private static class Node {
+        Object value;
+        Node next;
+
+        public Node(Object value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return ReflectionToStringBuilder.toString(this, ToStringStyle.JSON_STYLE);
+        }
     }
 }
